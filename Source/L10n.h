4a172665bd0d77a9e67a235520e2b69adad67205
Adrian Bobrowski <adrian071993@gmail.com>
2017-07-12 23:44:16 +0200

    Changed schemes for building and runing tests

--- Source/L10n.h
+++ Source/L10n.h
@@ -2,7 +2,7 @@
 //  L10n.h
 //
 //  Created by Adrian Bobrowski on 01.05.2017.
-//  Copyright (c) 2017 CodingLifestyle
+//  Copyright (c) 2017 Adrian Bobrowski (Decybel07), adrian071993@gmail.com. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
