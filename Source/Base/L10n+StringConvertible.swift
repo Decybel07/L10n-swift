1d5dbb72a913c9d0406a6ee518d9de473e2864a0
Adrian Bobrowski <adrian071993@gmail.com>
2017-07-30 00:18:44 +0200

    Implemented  and  for L10n class

---
+++ Source/Base/L10n+StringConvertible.swift
@@ -0,0 +1,24 @@
+//
+//  L10n+CustomStringConvertable.swift
+//  L10n
+//
+//  Created by Adrian Bobrowski on 29.07.2017.
+//  Copyright © 2017 Adrian Bobrowski (Decybel07), adrian071993@gmail.com. All rights reserved.
+//
+
+extension L10n: CustomStringConvertible {
+    public var description: String {
+        return "L10n(\(self.language))"
+    }
+}
+
+extension L10n: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        return "L10n {"
+            + "\n\tLanguage: \(self.language)"
+            + "\n\tPreferred languages: \(self.preferredLanguages)"
+            + "\n\tSupported languages: \(self.supportedLanguages)"
+            + "\n\tBundle: \(self.bundle?.bundlePath ?? "nil")"
+            + "\n\t}"
+    }
+}
