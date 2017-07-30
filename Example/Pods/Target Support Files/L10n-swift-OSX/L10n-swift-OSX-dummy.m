85af7e653b93309c068f44043f17c7db10d8b9d3
Adrian Bobrowski <adrian071993@gmail.com>
2017-07-30 20:43:38 +0200

    remove pods dictionary

--- Example/Pods/Target Support Files/L10n-swift-OSX/L10n-swift-OSX-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_L10n_swift_OSX : NSObject
-@end
-@implementation PodsDummy_L10n_swift_OSX
-@end
--- Example/Pods/Target Support Files/Pods-Example watchOS Extension/Pods-Example watchOS Extension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Example_watchOS_Extension : NSObject
-@end
-@implementation PodsDummy_Pods_Example_watchOS_Extension
-@end
